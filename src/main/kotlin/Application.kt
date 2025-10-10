8f95fbea94249f458d4a44681d6aedbe4c33cd27
Richard Balke <richardbalke@gmail.com>
2025-10-10 10:14:23 +0200

    Initial Commit

---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,14 @@
+package api
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureSecurity()
+    configureDatabases()
+    configureRouting()
+}
